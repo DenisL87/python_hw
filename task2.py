a16c97c5db602855ed02aa6d09b16d5fda349580
DenisL87 <68246621+DenisL87@users.noreply.github.com>
2021-03-17 13:19:33 +0300

    Update task2.py

--- task2.py
+++ task2.py
@@ -20,15 +20,8 @@ if count == 0:
 else:
   print("correct")
 # Checking for special sympols availability
-symbol_1 = string.find("$")
-symbol_2 = string.find("%")
-symbol_3 = string.find("_")
-if symbol_1 != -1 & symbol_2 != -1 & symbol_3 != -1:
+if string.find("$") != -1 & string.find("%") != -1 & string.find("_") != -1 & string.find(" ") == -1:
   print("well done")
 else:
-  print("invalid")
-  sys.exit(0)
-# Checking for gaps
-if string.find(" ") != -1:
   print("invalid")
   sys.exit(0)
