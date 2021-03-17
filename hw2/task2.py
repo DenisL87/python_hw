341950e86b26b49966aab4e19f08714aca487e1b
DenisL87 <denis.litvinenko@yahoo.com>
2021-03-17 21:18:42 +0200

    ...

--- hw2/task2.py
+++ hw2/task2.py
@@ -4,33 +4,43 @@ password = input("Type here your password: ")
 
 # Checking the password length
 if password.__len__() < 8:
-  print("Your password shall include at least 8 symbols")
-  sys.exit(0)
-  
+    print("Your password shall include at least 8 symbols")
+    sys.exit(0)
+
 # Checking for capital letters availability
-caps_count = 0
+iscapital = False
 for i in password:
-  if i.isupper():
-    caps_count += 1
-    break
-if caps_count == 0:
-  print ("Your password shall include at least one capital letter")
-  sys.exit(0)
-  
+    if i.isupper():
+        iscapital = True
+        break
+if not iscapital:
+    print("Your password shall include at least one capital letter")
+    sys.exit(0)
+
+# Checking for digits
+isdigit = False
+for i in password:
+    if i.isdigit():
+        isdigit = True
+        break
+if not isdigit:
+    print("Your password shall include at least one digit")
+    sys.exit(0)
+
 # Checking for special symbols
 symbols = ["!", "#", "$", "%", "^", "&", "*", "~"]
 count = 0
 while count < symbols.__len__():
-  if symbols[count] in password:
-    break
-  count += 1
-  if count == symbols.__len__():
-    print("You password shall include at least one of these symbols: !, #, $, %, ^, &, *, ~")
-    sys.exit(0)
-    
+    if symbols[count] in password:
+        break
+    count += 1
+    if count == symbols.__len__():
+        print("You password shall include at least one of these symbols: !, #, $, %, ^, &, *, ~")
+        sys.exit(0)
+
 # Checking for gaps
 if " " in password:
-  print("No gaps shall be used")
-  sys.exit(0)
+    print("No gaps shall be used")
+    sys.exit(0)
 else:
-  print("Your password has been validated successfully")
+    print("Your password has been validated successfully")
