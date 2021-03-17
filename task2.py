bf9424c45cf4b91a2b81415a6a687b30f4b0fbf2
DenisL87 <68246621+DenisL87@users.noreply.github.com>
2021-03-17 15:35:54 +0300

    Update task2.py

--- task2.py
+++ task2.py
@@ -26,3 +26,45 @@ else:
   else:
     print("No gaps shall be used")
   sys.exit(0)
+  
+  
+  
+  
+  
+  import sys
+
+password = input("type here your password: ")
+password_length: int = password.__len__()
+
+# Checking the password length
+if password_length < 8:
+  print("Your password shall include at least 8 sympols")
+  sys.exit(0)
+  
+# Checking for capital letters availability
+cap_count = 0
+for i in password:
+  if i.isupper():
+    cap_count += 1
+    break
+if cap_count == 0:
+  print ("Your password shall include at least one capital letter")
+  sys.exit(0)
+  
+# Checking for special symbols
+symbols = ["!", "#", "$", "%", "^", "&", "*", "~"]
+symb_count = 0
+while_count = 0
+while while_count < symbols.__len__():
+  if password.find(symbols[while_count]) != -1:
+    symb_count += 1
+    break
+  while_count += 1
+if symb_count > 0 and password.find(" ") == -1:
+  print("Your password has been validated successfully")
+elif symb_count == 0:
+  print("You password shall include at least one of these symbols: !, #, $, %, ^, &, *, ~")
+  sys.exit(0)
+else:
+  print("No gaps shall be used")
+  sys.exit(0)
