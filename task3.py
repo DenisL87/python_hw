51871d38e044e605ded2bce63118808a9e550304
DenisL87 <68246621+DenisL87@users.noreply.github.com>
2021-03-17 16:47:18 +0300

    Create task3.py

---
+++ task3.py
@@ -0,0 +1,38 @@
+import sys
+import math
+
+first_value: str = input('Input first value: ')
+operation: str = input('Input math operation: ')
+if operation == '**':
+  second_value = first_value
+  print(int(second_value) * int(first_value))
+  sys.exit(0)
+second_value: str = input('Input second value: ')
+
+for value in {first_value, second_value}:
+  if not value.isdigit():
+    print(f'Value of type {type(value)} {value} is not a digit')
+    sys.exit(0)
+  first_value: int = int(first_value)
+  second_value: int = int(second_value)
+   
+  if operation == '+':
+    print(first_value + second_value)
+  elif operation == '-':
+    print(first_value - second_value)
+  elif operation == '*':
+    print(first_value * second_value)
+  elif operation == '/':
+    if second_value == 0:
+      print("division by zero")
+      sys.exit(0)
+    else:
+      print(first_value / second_value)
+  elif operation == "//":
+    if second_value == 0:
+      print("division by zero")
+      sys.exit(0)
+    else:
+      print(math.floor(first_value / second_value))
+  else:
+    print(f'Invalid operation {operation}')
