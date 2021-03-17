ffc5d0cbda54b23ae9b5eecd1421c7f2694f9325
DenisL87 <68246621+DenisL87@users.noreply.github.com>
2021-03-17 18:22:16 +0300

    Update task3.py

--- task3.py
+++ task3.py
@@ -36,3 +36,44 @@ for value in {first_value, second_value}:
       print(math.floor(first_value / second_value))
   else:
     print(f'Invalid operation {operation}')
+
+    
+    import sys
+import math
+
+first_value: str = input('Input first value: ')
+if not first_value.isdigit():
+  print(f'Value of type {type(first_value)} {first_value} is not a digit')
+  sys.exit(0)
+operation: str = input('Input math operation: ')
+
+if operation == '**':
+  second_value = first_value
+  print(int(second_value) * int(first_value))
+  sys.exit(0)
+  
+second_value: str = input('Input second value: ')
+if not second_value.isdigit():
+  print(f'Value of type {type(second_value)} {second_value} is not a digit')
+  sys.exit(0)
+   
+if operation == '+':
+  print(first_value + second_value)
+elif operation == '-':
+  print(first_value - second_value)
+elif operation == '*':
+  print(first_value * second_value)
+elif operation == '/':
+  if second_value == 0:
+    print("division by zero")
+    sys.exit(0)
+  else:
+    print(first_value / second_value)
+elif operation == "//":
+  if second_value == 0:
+    print("division by zero")
+    sys.exit(0)
+  else:
+    print(math.floor(first_value / second_value))
+else:
+  print(f'Invalid operation {operation}')
