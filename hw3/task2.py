e2615fb73e25e0a82e30aacb6628d637a30c0d80
DenisL87 <denis.litvinenko@yahoo.com>
2021-03-20 19:37:33 +0200

    hw3

---
+++ hw3/task2.py
@@ -0,0 +1,22 @@
+list = []
+for i in range(187):
+    if i % 5 == 0 and i != 0:
+        list.append(i)
+print(list)
+
+max_el = list[0]
+for i in list:
+    if i > max_el:
+        max_el = i
+print(f'Max element: {max_el}')
+
+min_el = max_el
+for i in list:
+    if i < min_el and i != 0:
+        min_el = i
+print(f'Min element: {min_el}')
+
+sum = 0
+for i in list:
+    sum += i
+print(f'sum: {sum}')
