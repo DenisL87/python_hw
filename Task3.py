b57ef4dded903d3e0f71e013758f99371dd1e86a
DenisL87 <68246621+DenisL87@users.noreply.github.com>
2021-03-24 13:57:28 +0300

    Update Task3.py

--- Task3.py
+++ Task3.py
@@ -1,11 +1,19 @@
 import datetime
-year = input("Enter a year here")
+
+year = int(input("Enter a year here: "))
 month = int(1)
 day = int(1)
 mondays = []
-while year ==  
-  datetime.datetime(year, month, day)
-
-  if date_time.strftime("%a") == 'Tue':
-    print('Tuesday')
-#print(date_time.strftime("%a"))
+target_year = year
+while target_year == year:
+  while month < 13:
+    try:
+      date_time.datetime(year, month, day)
+      if date_time.strftime("%a") == 'Mon':
+        mondays.append(date_time.date_time("%b %d %Y %I:%M %p"))
+    except Exception:
+      month += 1
+  year += 1
+  
+  print(mondays)
+  
