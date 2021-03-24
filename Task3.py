2208139cd6ba59361a635fb532229271618aa880
DenisL87 <68246621+DenisL87@users.noreply.github.com>
2021-03-24 13:44:32 +0300

    Update Task3.py

--- Task3.py
+++ Task3.py
@@ -1,6 +1,11 @@
 import datetime
+year = input("Enter a year here")
+month = int(1)
+day = int(1)
+mondays = []
+while year ==  
+  datetime.datetime(year, month, day)
 
-year = int(input("Enter here a year: "))
-now = datetime.datetime.now()
-Mondays = []
-print(now.year)
+  if date_time.strftime("%a") == 'Tue':
+    print('Tuesday')
+#print(date_time.strftime("%a"))
