3ab28ebb6b5e160760d3b6ad39e01ffcaa1c0fe8
DenisL87 <68246621+DenisL87@users.noreply.github.com>
2021-03-24 17:32:08 +0300

    Update Task3.py

--- Task3.py
+++ Task3.py
@@ -3,7 +3,6 @@ import datetime
 year = int(input("Enter a year here: "))
 month = int(1)
 day = int(1)
-months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
 mondays = []
 
 while month < 13:
@@ -12,14 +11,11 @@ while month < 13:
   except ValueError as error:
     month += 1
     day = 1
+    continue
   if to_check.strftime('%a') == 'Mon':
     mondays.append(to_check.strftime("%Y-%m-%d %a"))
-  try:
-    if mondays[-1] == mondays[-2]:
-      del mondays[-1]
-  except:
-    pass
   day += 1
       
 for i in mondays:
   print (i)
+  
