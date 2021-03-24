12f5300d2f439a2e3f26962db082569fd9f0f680
DenisL87 <68246621+DenisL87@users.noreply.github.com>
2021-03-24 15:36:24 +0300

    Update Task3.py

--- Task3.py
+++ Task3.py
@@ -2,20 +2,22 @@ import datetime
 
 year = int(input("Enter a year here: "))
 month = int(1)
+months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
 day = int(1)
 mondays = []
 year_count = year
+to_check = datetime.datetime(year, month, day)
 
-while year_count == year:
-  to_check = datetime.datetime(year, month, day)
-  while month < 13:
-    try:
-      if datetime.strftime("%a") == 'Mon':
-        mondays.append(to_check.strftime("%Y-%m-%d %H:%M:%S %a"))
+while month < 13:
+  current_month = to_check.strftime('%b')
+  try:
+    for current_month in months:
+      to_check = datetime.datetime(year, month, day)
+      if to_check.strftime('%a') == 'Mon':
+        mondays.append(to_check.strftime("%Y-%m-%d %a"))
       day += 1
-    except Exception:
-      day = 1
-      month += 1
-  year_count += 1
+  except:
+    day = 1
+  month += 1
   
 print(mondays)
