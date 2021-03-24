ec49dc36473708faa05e4c2d90d658bc9c505147
DenisL87 <68246621+DenisL87@users.noreply.github.com>
2021-03-24 16:19:50 +0300

    Update Task3.py

--- Task3.py
+++ Task3.py
@@ -5,19 +5,16 @@ month = int(1)
 day = int(1)
 months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
 mondays = []
-year_count = year
-to_check = datetime.datetime(year, month, day)
 
 while month < 13:
-  current_month = to_check.strftime('%b')
   try:
-    for current_month in months:
-      to_check = datetime.datetime(year, month, day)
-      if to_check.strftime('%a') == 'Mon':
-        mondays.append(to_check.strftime("%Y-%m-%d %a"))
-      day += 1
-  except:
+    to_check = datetime.datetime(year, month, day)
+  except ValueError as error:
+    month += 1
     day = 1
-  month += 1
-  
-print(mondays)
+  if to_check.strftime('%a') == 'Mon':
+    mondays.append(to_check.strftime("%Y-%m-%d %a"))
+  day += 1
+      
+for i in mondays:
+  print (i)
