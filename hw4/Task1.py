4ba7494742ba9f475413153005594a88d6f61b7e
DenisL87 <denis.litvinenko@yahoo.com>
2021-03-24 23:20:20 +0200

    hw4

--- hw4/Task1.py
+++ hw4/Task1.py
@@ -3,7 +3,20 @@ string_file1 = file1.read()
 print(string_file1)
 file2 = open('file2.txt', mode='w')
 file2.write(string_file1)
-
-
-file1.close()
 file2.close()
+file1.close()
+
+# file1 = open('file1.txt', mode='r+')
+# new_string = file1.read().swapcase()
+# file1.write(new_string)
+# file1.close()
+# print(new_string)
+
+for i in file1:
+    
+
+# for i in new_string:
+#     if i.islower():
+#         new_string[i].swapcase()
+# file1.close()
+# print(new_string)
