0c0ce6e5864cf132b92028c8856d3cb214863882
DenisL87 <denis.litvinenko@yahoo.com>
2021-03-25 21:21:35 +0200

    hw4

--- hw4/Task1.py
+++ hw4/Task1.py
@@ -5,22 +5,7 @@ file2 = open('file2.txt', mode='w')
 file2.write(string_file1)
 file2.close()
 
-file1 = open('file1', mode='a')
+file1 = open('file1.txt', mode='a')
 string_file1 = string_file1.swapcase()
-file1.append('. ' + string_file1) # or file1.write('. ' + string_file1)
+file1.write(' / ' + string_file1)
 file1.close()
-
-# file1 = open('file1.txt', mode='r+')
-# new_string = file1.read().swapcase()
-# file1.write(new_string)
-# file1.close()
-# print(new_string)
-
-#for i in file1:
-    
-
-# for i in new_string:
-#     if i.islower():
-#         new_string[i].swapcase()
-# file1.close()
-# print(new_string)
