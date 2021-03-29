3c0b3f7132c20da5d599d5629499bfc478825bcf
DenisL87 <68246621+DenisL87@users.noreply.github.com>
2021-03-29 15:10:17 +0300

    Update Task1.py

--- hw4/Task1.py
+++ hw4/Task1.py
@@ -11,3 +11,14 @@ string_file1 = string_file1.swapcase()
 file1 = open('file1.txt', mode='a')
 file1.write(' / ' + string_file1)
 file1.close()
+
+# with open('file1.txt', mode='r') as file1:
+#   string_file1 = file1.read()
+# print(string_file1)
+
+# with open('file2.txt', mode='w') as file2:
+#   file2.write(string_file1)
+
+# string_file1 = string_file1.swapcase()
+# with open('file1.txt', mode='a') as file1:
+#   file1.write(' / ' + string_file1)
