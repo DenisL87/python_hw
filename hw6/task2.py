d5906d1d7572a2ac894f7a947d472753ac8db4b1
DenisL87 <denis.litvinenko@yahoo.com>
2021-03-31 20:50:05 +0300

    hw6

--- hw6/task2.py
+++ hw6/task2.py
@@ -1,12 +1,3 @@
-# def is_even(integer):
-#   if integer %2 == 0:
-#     return True
-#   else:
-#     return False
-
-# if __name__ == "__main__":
-#   print(is_even(8))
-  
 def is_even(integer):
   if integer % 2 == 0 and integer != 0:
     return True
