8417613d73f569c2e6c5d2f98cbb72fb029a58a5
DenisL87 <68246621+DenisL87@users.noreply.github.com>
2021-03-31 09:30:28 +0300

    Create task3.py

---
+++ task3.py
@@ -0,0 +1,13 @@
+def is_prime(integer):
+  count = 2
+  if integer > 1:
+    while count < integer - 1:
+      if integer % count == 0:
+        return False
+      count += 1
+    return True
+  else:
+    return False
+
+if __name__ == "__main__":
+  print(is_prime(17))
