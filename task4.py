e21c4f1fe2887bb6c18af92bdb8b12e98cc9cfd2
DenisL87 <68246621+DenisL87@users.noreply.github.com>
2021-03-31 12:00:24 +0300

    Update task4.py

--- task4.py
+++ task4.py
@@ -1,5 +1,17 @@
-def string_processing(string):
-  
+def string_processing(instructions):
+  list = []
+  init_no = 0
+  for operation in instructions:
+    if operation == 'i':
+      init_no += 1
+    elif operation == 'd':
+      init_no -= 1
+    elif operation == 's':
+      init_no *= init_no
+    elif operation == 'o':
+      list.append(init_no)
+  return list
 
 if __name__ == "__main__":
+  print(string_processing('iiisdoso'))
   
