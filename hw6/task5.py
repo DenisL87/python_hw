01f15cee846a23f6f144aca87535f2ee48e614c6
DenisL87 <68246621+DenisL87@users.noreply.github.com>
2021-04-01 10:07:08 +0300

    Update task5.py

--- hw6/task5.py
+++ hw6/task5.py
@@ -11,3 +11,18 @@ def edit_file(file_name):
 
 if __name__ == '__main__':
     print(open_file("questions.json"))
+    
+    
+    
+# def edit_file(dict):
+#   count = 0
+#   while count < len(dict):
+#     print(dict[count]['q'])
+#     string = input('Type here: ')
+#     dict[count]['a'] = string
+#     count += 1
+#   return dict
+  
+# if __name__ == "__main__":
+#   dict = [{'q': 'What is your name?', 'a': ''}, {'q': 'What is your age?', 'a': ''}]
+#   edit_file(dict)
