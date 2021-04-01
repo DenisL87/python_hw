c229d17d8dc7035a300395ca39d3773a9f293c2b
DenisL87 <68246621+DenisL87@users.noreply.github.com>
2021-04-01 21:54:03 +0300

    Update task5.py

--- hw6/task5.py
+++ hw6/task5.py
@@ -1,47 +1,21 @@
 import json
-def open_file(file_name):
-    with open(file_name) as file:
-        data = json.load(file)
-    return data
 
 def edit_file(file_name):
+    answers = []
+    with open(file_name) as file:
+        data = json.load(file)
+        count = 0
+        while count < len(data["questions"]):
+            print(data["questions"][count]["q"])
+            answers.append(input("Type your answer: "))
+            count += 1
+
     with open(file_name, 'w') as file:
-        file.write(json.dumps('Hello'))
-    return True
+        count = 0
+        while count < len(data["questions"]):
+            file["questions"][count]["answer"].write(json.dump(answers[count]))
+    return file_name
 
 if __name__ == '__main__':
-    print(open_file("questions.json"))
+    edit_file("questions.json")
     
-    
-    
-# def edit_file(dict):
-#   count = 0
-#   while count < len(dict):
-#     print(dict[count]['q'])
-#     string = input('Type here: ')
-#     dict[count]['a'] = string
-#     count += 1
-#   return dict
-  
-# if __name__ == "__main__":
-#   dict = [{'q': 'What is your name?', 'a': ''}, {'q': 'What is your age?', 'a': ''}]
-#   edit_file(dict)
-
-
-
-
-
-# def edit_file(dict):
-#   count = 0
-#   while count < len(dict):
-#     print(dict[count]['q'])
-#     string = input('Type here: ')
-#     dict[count]['a'] = string
-#     print(dict[count]['a'])
-#     count += 1
-#   return dict
-  
-# if __name__ == "__main__":
-#   dict = [{'q': 'What is your name?', 'a': ''}, {'q': 'What is your age?', 'a': ''}]
-#   edit_file(dict)
-#   print("Game over")
