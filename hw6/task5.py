6eb234edd2aa34b1a3c32d18c3db9bab90d8f46a
DenisL87 <denis.litvinenko@yahoo.com>
2021-04-01 23:16:50 +0300

    hw6

--- hw6/task5.py
+++ hw6/task5.py
@@ -1,16 +1,17 @@
-import json
+ import json
 
 def edit_file(file_name):
     with open(file_name) as file:
         data = json.load(file)
-        count = 0
-        while count < len(data["questions"]):
-            print(data["questions"][count]["q"])
-            data["questions"][count]["answer"] = input("Type your answer: ")
-            count += 1
+    count = 0
+    while count < len(data["questions"]):
+        print(data["questions"][count]["q"])
+        data["questions"][count]["answer"] = input("Type your answer: ")
+        count += 1
     with open(file_name, 'w') as file:
         json.dump(data, file, indent=4)
     return file
 
 if __name__ == '__main__':
-    edit_file("questions.json")    
+    edit_file("questions.json")
+    
