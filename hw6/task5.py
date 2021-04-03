cba7cfddc9b7f6bb4cce23b1a7296a18ba7a631e
DenisL87 <68246621+DenisL87@users.noreply.github.com>
2021-04-03 14:08:03 +0300

    Update task5.py

--- hw6/task5.py
+++ hw6/task5.py
@@ -3,15 +3,13 @@ import json
 def edit_file(file_name):
     with open(file_name) as file:
         data = json.load(file)
-    count = 0
-    while count < len(data["questions"]):
-        print(data["questions"][count]["q"])
-        data["questions"][count]["answer"] = input("Type your answer: ")
-        count += 1
+    
+    for idx, q in enumerate(data.get('questions', [])):
+        data['questions'][idx]['answer'] = input(q['q'])
+        
     with open(file_name, 'w') as file:
         json.dump(data, file, indent=4)
-    return file
-
+        
 if __name__ == '__main__':
     edit_file("questions.json")
     
