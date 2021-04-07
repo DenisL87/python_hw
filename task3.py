984e5cd9a2282f59261d9703b04319b72b1b7174
DenisL87 <68246621+DenisL87@users.noreply.github.com>
2021-04-07 12:00:21 +0300

    Update task3.py

--- task3.py
+++ task3.py
@@ -7,9 +7,12 @@ def converter(string, separator):
     while p_count < len(punctuation):
       if punctuation[p_count] in new_list[count]:
         new_list[count] = new_list[count].replace(punctuation[p_count], '')
+        if new_list[count] == None:
+          new_list.pop(new_list[count])
       p_count += 1
     count += 1
-  print(new_list)
+  
+  
   pass
 
 
