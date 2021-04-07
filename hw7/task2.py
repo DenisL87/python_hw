d14ef6ea19a6e029ec98cf829329395dc97194ce
DenisL87 <denis.litvinenko@yahoo.com>
2021-04-07 23:08:45 +0300

    hw7

--- hw7/task2.py
+++ hw7/task2.py
@@ -16,16 +16,16 @@ def main():
     start = input('Start game yes|no')
     player_1_sum = 0
     player_2_sum = 0
-    new_dict = {'Round 1': {'player1name': "Player1", 'player2name': "Player2", 'player1res': None, 'player2res': None},
-                'Round 2': {'player1name': "Player1", 'player2name': "Player2", 'player1res': None, 'player2res': None},
-                'Round 3': {'player1name': "Player1", 'player2name': "Player2", 'player1res': None, 'player2res': None},
-                'Round 4': {'player1name': "Player1", 'player2name': "Player2", 'player1res': None, 'player2res': None},
-                'Round 5': {'player1name': "Player1", 'player2name': "Player2", 'player1res': None, 'player2res': None},
-                'Round 6': {'player1name': "Player1", 'player2name': "Player2", 'player1res': None, 'player2res': None},
-                'Round 7': {'player1name': "Player1", 'player2name': "Player2", 'player1res': None, 'player2res': None},
-                'Round 8': {'player1name': "Player1", 'player2name': "Player2", 'player1res': None, 'player2res': None},
-                'Round 9': {'player1name': "Player1", 'player2name': "Player2", 'player1res': None, 'player2res': None},
-                'Round 10': {'player1name': "Player1", 'player2name': "Player2", 'player1res': None, 'player2res': None},
+    new_dict = {'Round 1': {'player_1_name': "Player1", 'player_1_time': None, 'player_1_res': None, 'player_2_name': "Player2", 'player_2_time': None, 'player_2_res': None},
+                'Round 2': {'player_1_name': "Player1", 'player_1_time': None, 'player_1_res': None, 'player_2_name': "Player2", 'player_2_time': None, 'player_2_res': None},
+                'Round 3': {'player_1_name': "Player1", 'player_1_time': None, 'player_1_res': None, 'player_2_name': "Player2", 'player_2_time': None, 'player_2_res': None},
+                'Round 4': {'player_1_name': "Player1", 'player_1_time': None, 'player_1_res': None, 'player_2_name': "Player2", 'player_2_time': None, 'player_2_res': None},
+                'Round 5': {'player_1_name': "Player1", 'player_1_time': None, 'player_1_res': None, 'player_2_name': "Player2", 'player_2_time': None, 'player_2_res': None},
+                'Round 6': {'player_1_name': "Player1", 'player_1_time': None, 'player_1_res': None, 'player_2_name': "Player2", 'player_2_time': None, 'player_2_res': None},
+                'Round 7': {'player_1_name': "Player1", 'player_1_time': None, 'player_1_res': None, 'player_2_name': "Player2", 'player_2_time': None, 'player_2_res': None},
+                'Round 8': {'player_1_name': "Player1", 'player_1_time': None, 'player_1_res': None, 'player_2_name': "Player2", 'player_2_time': None, 'player_2_res': None},
+                'Round 9': {'player_1_name': "Player1", 'player_1_time': None, 'player_1_res': None, 'player_2_name': "Player2", 'player_2_time': None, 'player_2_res': None},
+                'Round 10': {'player_1_name': "Player1", 'player_1_time': None, 'player_1_res': None, 'player_2_name': "Player2", 'player_2_time': None, 'player_2_res': None}
                 }
     if start == 'yes':
         counter = 1
@@ -38,8 +38,10 @@ def main():
                 continue
             player_1_sum += player_1_value
             player_2_sum += player_2_value
-            new_dict['Round ' + str(counter)]['player1res'] = player_1_value
-            new_dict['Round ' + str(counter)]['player2res'] = player_2_value
+            new_dict['Round ' + str(counter)]['player_1_time'] = str(datetime.now())
+            new_dict['Round ' + str(counter)]['player_1_res'] = player_1_value
+            new_dict['Round ' + str(counter)]['player_2_time'] = str(datetime.now())
+            new_dict['Round ' + str(counter)]['player_2_res'] = player_2_value
             counter += 1
 
         with open('new_file.json', 'w') as file:
