4bbf38f208dfc22390914322567bc71cb26e1da0
DenisL87 <68246621+DenisL87@users.noreply.github.com>
2021-04-21 15:27:29 +0300

    Update group.py

--- group.py
+++ group.py
@@ -12,30 +12,37 @@ class Group():
   def expell_student(self, student):
       self.__students.remove(student)
   
+  def student_average_grade(self, grades):
+    sum = 0
+    count = 0
+    while count < len(grades):
+      sum += grades[count]
+      count += 1
+    return sum / len(grades)
+    
   def group_average_grade(self):
     if len(self.__students) > 0:
       sum = 0
       count = 0
       while count < len(self.__students):
-        sum += self.__students[count].grade
+        sum += self.student_average_grade(self.__students[count].grades)
         count += 1
       return sum / len(self.__students)
     else:
       return'There are no students in the group'
 
 class Student():
-  def __init__(self, name, age, grade):
+  def __init__(self, name, age, grades):
     self.name = name
     self.age = age
-    self.grade = grade
+    self.grades = grades
 
 group = Group("Group A")
-stud = Student('Vova', 17, 5)
-stud1 = Student('Vasya', 18, 8)
+stud = Student('Vova', 17, [5, 7])
+stud1 = Student('Vasya', 18, [8, 3])
 group.add_student(stud)
 group.add_student(stud1)
 print(group.get_students())
-print(group.group_average_grade())
-group.expell_student(stud1)
-print(group.get_students())
+print(group.student_average_grade(stud.grades))
+print(group.student_average_grade(stud1.grades))
 print(group.group_average_grade())
