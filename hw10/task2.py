e25bf909bb6b4fe1af8190fcc4f1269f775e545f
DenisL87 <68246621+DenisL87@users.noreply.github.com>
2021-04-21 09:09:50 +0300

    Update task2.py

--- hw10/task2.py
+++ hw10/task2.py
@@ -24,3 +24,42 @@ p2 = Person("afgadg", 62)
 p.know(p2)
 print(p.is_known(p2))
 print(p2.is_known(p))
+
+
+
+import uuid
+
+class Person:
+    def __init__(self, age, name, _id):
+        self.name = name
+        self.age = age
+        self._id = _id
+        self.__friends = {}
+
+    def know(self, other):
+        self.__friends.setdefault(other._id, other)
+
+    def is_known(self, other):
+        return other._id in self.__friends
+
+    @property
+    def friends(self):
+        return list(self.__friends.values())
+
+
+person1 = Person(18, 'Oleg', _id=uuid.uuid4())
+person2 = Person(20, 'Ivan', _id=uuid.uuid4())
+person3 = Person(20, 'Ivan', _id=uuid.uuid4())
+
+print(person1.is_known(person2))
+
+person1.know(person2)
+person1.know(person2)
+person1.know(person2)
+person1.know(person2)
+person1.know(person2)
+person1.know(person3)
+print(person1.is_known(person2))
+print(person2.is_known(person3))
+print(person1.friends)
+
