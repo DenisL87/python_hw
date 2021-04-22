1d17073e5c94e7fd9cf1023ad7f1550fd2389c2e
DenisL87 <68246621+DenisL87@users.noreply.github.com>
2021-04-22 09:30:25 +0300

    Update group.py

--- group.py
+++ group.py
@@ -7,10 +7,15 @@ class Group():
     return self.__students
   
   def add_student(self, student):
-    self.__students.append(student)
+    if student.group == None:
+      self.__students.append(student)
+      student.assign_group(self)
+    else:
+      print(f'The student is already in the {student.group.name}')
     
   def expell_student(self, student):
       self.__students.remove(student)
+      student.group = None
   
   def student_average_grade(self, grades):
     sum = 0
@@ -36,12 +41,19 @@ class Student():
     self.name = name
     self.age = age
     self.grades = grades
+    self.group = None
+  
+  def assign_group(self, group):
+    self.group = group
 
 group_a = Group("Group A")
+group_b = Group("Group B")
 stud1 = Student('Vova', 17, [5, 7])
+print(stud1.group)
 stud2 = Student('Vasya', 18, [8, 3])
 group_a.add_student(stud1)
 group_a.add_student(stud2)
+group_b.add_student(stud1)
 print(group_a.get_students())
 print(group_a.student_average_grade(stud1.grades))
 print(group_a.student_average_grade(stud2.grades))
